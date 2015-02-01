7a29f9d90bbb71ed2c88c52d7f5dca36c7483c7d
emu4hire <emu4hire@gmail.com>
2015-02-01 18:16:12 -0500

    iterative minmax & makefile changes

---
+++ a0/my_iter_minmax.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void minmax(int[], int);
+
+int main(int argc, char * args[]){
+	
+	ifstream in;
+	in.open(args[1]);
+	
+	int length;	
+	
+	in >> length;
+	cout <<length<<endl;
+
+	int input[length];
+
+	for(int i=0; i< length; i++){
+		in >> input[i];
+	}
+
+	minmax(input, length);
+	
+
+	in.close();
+
+
+}
+
+void minmax(int numbers[], int len){
+
+	int max=numbers[0];
+	int min=numbers[0];
+	int count=0;
+	
+	for(int i=1; i < len; i++){
+		if(numbers[i] > max){
+			max = numbers[i];
+		}	
+
+		if(numbers[i] < min){
+			min = numbers[i];
+		}
+		count +=2;
+	}
+
+	cout<<"The max is: "<<max<<"."<<endl;
+	cout<<"The min is: "<<min<<"."<<endl;
+	cout<<"The number of array element comparisons is: "<<count<<"."<<endl;
+}
