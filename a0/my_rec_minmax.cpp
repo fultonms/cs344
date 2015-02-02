e06b06400dae7ae437a515c1465d6e5ca5cd35d6
emu4hire <emu4hire@gmail.com>
2015-02-02 12:28:21 -0500

    recursive minmax, same and iterative. Probably not right

--- a0/my_rec_minmax.cpp
+++ a0/my_rec_minmax.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void minmax(int, int [], int, int &, int &, int &);
+
 int main(int argc, char * args[]){
 	
 	ifstream in;
@@ -19,9 +21,42 @@ int main(int argc, char * args[]){
 		in >> input[i];
 	}
 	
-
+	int min=input[0];
+	int max=input[0];
+	int count = 0;
+	minmax(1, input, length, min, max, count);
+	
+	cout<<"The max is: "<<max<<"."<<endl;
+	cout<<"The min is: "<<min<<"."<<endl;
+	cout<<"The number of count operations is: "<<count<<"."<<endl;
 
 	in.close();
 
 
 }
+
+void minmax(int i, int numbers[], int len, int & min, int & max, int & count){
+
+	if(i==len)
+		return;
+	else{
+		count++;
+		if(numbers[i] > max){
+			max=numbers[i];
+		}
+		else{
+			count ++;
+			if(numbers[i]< min){
+				min=numbers[i];
+			}
+
+		}
+
+	}
+	i++;
+	minmax(i, numbers, len, min, max, count);
+	return;
+}
+
+
+
