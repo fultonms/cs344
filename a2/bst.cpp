bd73f46fba8d26678737fd149ad2757cf55fefa5
emu4hire <emu4hire@gmail.com>
2015-04-19 19:59:43 -0400

    added a2 & a3

---
+++ a2/bst.cpp
@@ -0,0 +1,18 @@
+template <class T>
+
+BST<T> * search(const T &value){
+
+}
+
+BST<T> * insert(const T &value){
+	if(this.value > value)
+		*right.insert(value);
+	else if(this.value < value)
+		*left->instert(value);
+	else
+		this.value = value;
+}
+
+BST<T> * delete(const T &value){
+
+}
